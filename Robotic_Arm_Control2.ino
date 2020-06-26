37d254046e612e89e4d4fd8a66e470cded4837ed
MattR-0 <67388953+MattR-0@users.noreply.github.com>
2020-06-26 14:23:42 -0700

    Update Robotic_Arm_Control2.ino

--- Robotic_Arm_Control2.ino
+++ Robotic_Arm_Control2.ino
@@ -1,15 +1,11 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
-Servo base;
-Servo claw;
-Servo elbow;
-Servo wrist;
+Servo base, claw, elbow, wrist;
 SoftwareSerial Bluetooth(12, 11); //Assigns the HC-06 bluetooth module to its connected Arduino RX, TX pins
-int baseDef = 90, clawDef = 70, elbowDef = 70, wristDef = 90; //Sets default servo positions
 int basePPos = 90, clawPPos = 70, elbowPPos = 70, wristPPos = 90; //PPos is current servo position
 int basePos = 90, clawPos = 70, elbowPos = 70, wristPos = 90; //Pos will be used as future servo position
 double speed = 1.0;
-String phoneOutput = "";
+String phoneOutput = "", outputN = "", outputX = "";
 
 void setup() {
   base.attach(9); //Assigns each servo to its corresponding pin on the Arduino
@@ -19,25 +15,30 @@ void setup() {
   Bluetooth.begin(9600); //Sets bluetooth baud rate to 9600 bps (same as Android tablet)
   Bluetooth.setTimeout(1); //Sets bluetooth timeout time to 1 ms; Arduino will check for bluetooth data for 1ms before interpreting it
   delay(20);
-  base.write(baseDef); //Sets each servo's position to default
-  claw.write(clawDef);
-  elbow.write(elbowDef);
-  wrist.write(wristDef);
+  base.write(basePPos); //Sets each servo's position to default
+  claw.write(clawPPos);
+  elbow.write(elbowPPos);
+  wrist.write(wristPPos);
+  Serial.begin(9600);
 }
 
 //Repeatedly checks for bluetooth signals and moves the servos based on the signals
 void loop() {
   if (Bluetooth.available()) { //Detects if data is being sent over bluetooth
     phoneOutput = Bluetooth.readString(); //Reads incoming bluetooth data as a String and assigns it to phoneOutput
-    String outputN = "", outputX = "";
-    while (phoneOutput.indexOf(';') != -1) {
+    Serial.println("phoneOutput " + phoneOutput);
+    while (phoneOutput.indexOf(';') != -1) { //If the data contains a semicolon
+      if (phoneOutput.charAt(0) <= 122 && phoneOutput.charAt(0) >= 97) //If a new command is sent after a command was cut off
+        outputX = ""; //Clears the storage to prevent the cut off command from interfering with the new one
       outputN = outputX + phoneOutput.substring(0,phoneOutput.indexOf(';'));
       phoneOutput = phoneOutput.substring(phoneOutput.indexOf(';')+1,phoneOutput.length());
+      Serial.println("outputN " + outputN);
       switch (char n = outputN.charAt(0)) {
         case ('b'):
           basePos = outputN.substring(1,outputN.length()).toInt(); //Sets the future base position to the position sent over bluetooth
           changePos(basePPos,basePos,base); //Moves base from current position to future position
           basePPos = base.read(); //Updates the current base position
+          Serial.println(basePos);
           break;
         case ('c'): //Same as above, but for the claw servo
           clawPos = outputN.substring(1,outputN.length()).toInt();
@@ -60,6 +61,7 @@ void loop() {
       }
     }
     outputX = phoneOutput;
+    Serial.println("outputX " + outputX);
   }
 }
 
@@ -68,13 +70,13 @@ void changePos(int PPos, int Pos, Servo servo) {
   if (PPos > Pos) {
     for (int i = PPos; i >= Pos; i--) {
       servo.write(i);
-      delay((int) (30/speed));
+      delay((int) (20/speed));
     }
   }
   else if (PPos < Pos) {
     for (int i = PPos; i <= Pos; i++) {
       servo.write(i);
-      delay((int) (30/speed));
+      delay((int) (20/speed));
     }
   }
 }
